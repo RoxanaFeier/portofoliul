1235ce249974bc04d4d037c4e8fe988e334f7569
RoxanaFeier <roxanafeier6@gmail.com>
2025-11-13 15:57:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// Cache the DOM
+var button = document.getElementById("button")
+var nume = document.getElementById("nume")
+var prenume = document.getElementById("prenume")
+var educatie = document.getElementById("educatie")
+var img = document.query.querySelector("img")
+var body = document.query.querySelector("body")
+var an = document.getElementById("anNastere")
+
+// Add event listener
+button.adEventListener("click", altaViata)
+
+//Define function
+function altaViata() {
+         nume.innerHTML = "Software Engineer"
+         prenume.innerHTML = "Google"
+}
+
+educatie. innerHTML = "Experiente"
+asteptari. innerHTML = "‹ul>li›orkshops</li>
+‹li>Internship‹/li></ul>"
+
+     img.src = "images/lion.jpg"
+     img. style.opacity = "70%"
+     img. style.....
+     educatie.innerHTML = "Experiente"
+     educatie, style. fontSize = "50px"
+     asteptari. innerHTML = "‹ul>‹li>
+     astronomice‹/lili>trigonometrice‹/lix/ul>"
+     body. style.backgroundImage =
+     "url ('images/field.jpg')"
+     body. style.backgroundSize = "cover"
+     body, style. backgroundRepeat = "no-repeat"
+     body. style.backgroundPosition = "center"
+     body. style.backgroundAttachment = "fixed"
+     body. style.color = "yellow"
+     body. style. fontFamily = "Montserrat"
+     img. src = "images/lion.jpg"
+     img. style.opacity = "70%"
+     img. style.border = "5px solid black"
